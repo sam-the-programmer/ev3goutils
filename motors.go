8eee38024a2b1ff7014ea10d72344a8e91dd706f
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-12-05 16:51:59 +0000

    Added newError functions

--- motors.go
+++ motors.go
@@ -8,16 +8,22 @@ import (
 	"github.com/ev3go/ev3dev"
 )
 
+func newError(message string) error {
+	return errors.New("\033[31m ERROR: " + message + "\033[0m")
+}
+
 type MotorSet struct {
 	A ev3dev.TachoMotor // Motor at the A port.
 	B ev3dev.TachoMotor // Motor at the B port.
 	C ev3dev.TachoMotor // Motor at the C port.
 	D ev3dev.TachoMotor // Motor at the D port.
+
+	connectedMotors []string // Slice of connected motors.
 }
 
 func (m *MotorSet) connectMotor(port string, size string) {
 	if strings.ToLower(size) != "l" && strings.ToLower(size) != "m" {
-		log.Fatal(errors.New("Motor size" + strings.ToLower(size) + "is not a valid motor size. Use l or m"))
+		log.Fatal(newError("Motor size" + strings.ToLower(size) + "is not a valid motor size. Use l or m"))
 	}
 	motorType := "lego-ev3-" + strings.ToLower(size) + "-motor"
 
@@ -25,17 +31,34 @@ func (m *MotorSet) connectMotor(port string, size string) {
 	case "A":
 		motor, _ := ev3dev.TachoMotorFor("ev3-ports:outA", motorType)
 		m.A = *motor
+		m.connectedMotors = append(m.connectedMotors, "a")
 	case "B":
 		motor, _ := ev3dev.TachoMotorFor("ev3-ports:outB", motorType)
 		m.B = *motor
+		m.connectedMotors = append(m.connectedMotors, "b")
 	case "C":
 		motor, _ := ev3dev.TachoMotorFor("ev3-ports:outC", motorType)
 		m.C = *motor
+		m.connectedMotors = append(m.connectedMotors, "c")
 	case "D":
 		motor, _ := ev3dev.TachoMotorFor("ev3-ports:outD", motorType)
 		m.D = *motor
+		m.connectedMotors = append(m.connectedMotors, "d")
 	default:
-		log.Fatal(errors.New("Port" + strings.ToUpper(port) + "is not a valid motor port name. Use A, B, C or D."))
+		log.Fatal(newError("Port " + strings.ToUpper(port) + " is not a valid motor port name. Use A, B, C or D."))
+	}
+}
+
+func (m *MotorSet) checkMotor(motor string) {
+	in := false
+	for _, v := range m.connectedMotors {
+		if v == strings.ToLower(motor) {
+			in = true
+		}
+	}
+
+	if !in {
+		log.Fatal(newError("Motor \"" + motor + "\" is not a valid motor name or is not connected."))
 	}
 }
 
@@ -51,10 +74,18 @@ func (m *MotorSet) runDegrees(motor *ev3dev.TachoMotor, degrees int, speed int) 
 	motor.SetPositionSetpoint(degrees).Command("run-to-rel-pos")
 }
 
-func (m *MotorSet) ADegrees(degrees int, speed int) { m.runDegrees(&m.A, degrees, speed) }
-func (m *MotorSet) BDegrees(degrees int, speed int) { m.runDegrees(&m.B, degrees, speed) }
-func (m *MotorSet) CDegrees(degrees int, speed int) { m.runDegrees(&m.C, degrees, speed) }
-func (m *MotorSet) DDegrees(degrees int, speed int) { m.runDegrees(&m.D, degrees, speed) }
+func (m *MotorSet) ADegrees(degrees int, speed int) {
+	m.runDegrees(&m.A, degrees, speed)
+}
+func (m *MotorSet) BDegrees(degrees int, speed int) {
+	m.runDegrees(&m.B, degrees, speed)
+}
+func (m *MotorSet) CDegrees(degrees int, speed int) {
+	m.runDegrees(&m.C, degrees, speed)
+}
+func (m *MotorSet) DDegrees(degrees int, speed int) {
+	m.runDegrees(&m.D, degrees, speed)
+}
 
 func NewMotorSet(ports []string) MotorSet {
 	m := MotorSet{}
