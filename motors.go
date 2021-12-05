6e13d88f315bc65c24df074867abaee664049354
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-12-05 10:11:23 +0000

    Finish basic motor functions

--- motors.go
+++ motors.go
@@ -46,6 +46,16 @@ func (m *MotorSet) ConnectMediumMotor(port string) {
 	m.connectMotor(port, "m")
 }
 
+func (m *MotorSet) runDegrees(motor *ev3dev.TachoMotor, degrees int, speed int) {
+	motor.SetSpeedSetpoint(speed)
+	motor.SetPositionSetpoint(degrees).Command("run-to-rel-pos")
+}
+
+func (m *MotorSet) ADegrees(degrees int, speed int) { m.runDegrees(&m.A, degrees, speed) }
+func (m *MotorSet) BDegrees(degrees int, speed int) { m.runDegrees(&m.B, degrees, speed) }
+func (m *MotorSet) CDegrees(degrees int, speed int) { m.runDegrees(&m.C, degrees, speed) }
+func (m *MotorSet) DDegrees(degrees int, speed int) { m.runDegrees(&m.D, degrees, speed) }
+
 func NewMotorSet(ports []string) MotorSet {
 	m := MotorSet{}
 	return m
